5b86a8f2ba52810696e18d6eafa94a83fc3cc0c3
mistermk24 <aleksandr.seleznev24@gmail.com>
2020-10-08 22:35:11 +0200

    devise_token_auth

--- rambler_topics/app/models/user.rb
+++ rambler_topics/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  extend Devise::Models
   has_many :authored_topics, class_name: "Topic", foreign_key: 'author_id'
   validates :username, presence: true, 
                        uniqueness: true
   validates :email, presence: true
+
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
 end
